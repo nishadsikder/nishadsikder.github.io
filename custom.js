094045ddfb0b9d5874aa044e98903060a7c77788
Nishad Sikder <68532026+nishadsikder@users.noreply.github.com>
2020-10-10 01:44:47 +0600

    Add files via upload
    yuyuy

---
+++ custom.js
@@ -0,0 +1,19 @@
+
+var navb = document.getElementById("navbar-nav");
+var navlink = navb.getElementsByClassName("nav-link");
+for (var i = 0; i < navlink.length; i++) {
+  navlink[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}
+
+  // Preloader
+  $(window).on('load', function() {
+    if ($('#preloader').length) {
+      $('#preloader').delay(500).fadeOut('slow', function() {
+        $(this).remove();
+      });
+    }
+  });
